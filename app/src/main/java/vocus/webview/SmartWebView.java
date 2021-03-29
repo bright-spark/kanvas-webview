320c42344d85426d205477616a7d4b533fad3ae0
martin myburgh <bright-spark@users.noreply.github.com>
2021-03-29 12:40:02 +0200

    Update SmartWebView.java

--- app/src/main/java/vocus/webview/SmartWebView.java
+++ app/src/main/java/vocus/webview/SmartWebView.java
@@ -32,7 +32,7 @@ class SmartWebView {
 
 	// show ratings dialog; auto configured
 	// edit method get_rating() for customizations
-	static boolean ASWP_RATINGS     = false;
+	static boolean ASWP_RATINGS     = true;
 
 	// pull refresh current url
 	static boolean ASWP_PULLFRESH	 = false;
@@ -62,7 +62,7 @@ class SmartWebView {
 	/* -- CONFIG VARIABLES -- */
 
 	//complete URL of your website or offline webpage
-	static String ASWV_URL          = "https://kanvas.koding.co.za/var/apps/browser/index-prod.html#/overview";
+	static String ASWV_URL          = "http://kanvas.koding.co.za/var/apps/browser/index-prod.html#/overview";
 
 	//to upload any file type using "*/*"; check file type references for more
 	static String ASWV_F_TYPE       = "*/*";
